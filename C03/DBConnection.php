37d37d8ee9e6701dc122736db0e43a6ba2adfe94
RIYA VINCENT <74886881+riyavincent@users.noreply.github.com>
2021-03-14 23:29:54 +0530

    COURSE OUTCOME 2 ORIGINAL

---
+++ C03/DBConnection.php
@@ -0,0 +1,9 @@
+
+<?php
+ //Establishing connection with the database
+ define('DB_SERVER', 'localhost:3306');
+ define('DB_USERNAME', 'root');
+ define('DB_PASSWORD', '');
+ define('DB_DATABASE', 'riya'); //where profile is the database 
+ $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
